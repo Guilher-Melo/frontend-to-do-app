d31b99e369c02e534da202ab01b86660342e571e
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2023-05-05 13:56:01 -0300

    Removido /about

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Todo from "./todo/Todo";
-import About from "./about/About";
 
 
 export default function Rotas(props) {
   return(
     <Routes>
       <Route path="/todos" Component={Todo} />
-      <Route path="/about" Component={About} />
       <Route path="*" Component={Todo} />
     </Routes>
   )
